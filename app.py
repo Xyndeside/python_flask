7c6a561fca1d3ca496ad84724ae08c6cf9d50d97
Daniil Koval <Daniilkoval2006@mail.ru>
2025-04-30 19:08:46 +1000

    Добавление аутентификации

--- app.py
+++ app.py
@@ -1,25 +1,83 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect, flash
+from flask_login import LoginManager, login_required, current_user, login_user, logout_user
 from flask_migrate import Migrate
 
-from database.models import db, Student
+from database.models import db, Student, User
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'super-secret'
 
 db.init_app(app)
 migrate = Migrate(app, db)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@login_required
+def profile():
+    return render_template('profile.html')
+
 
 @app.route('/students')
+@login_required
 def get_students():
     students = Student.query.all()
     return render_template('students.html', students=students)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('profile'))
+
+        flash('Неверные имя пользователя или пароль')
+
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('Пользователь с таким именем уже существует')
+            return redirect(url_for('register'))
+
+        new_user = User(username=username)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        login_user(new_user)
+        return redirect(url_for('profile'))
+
+    return render_template('register.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 
 def run_app():
